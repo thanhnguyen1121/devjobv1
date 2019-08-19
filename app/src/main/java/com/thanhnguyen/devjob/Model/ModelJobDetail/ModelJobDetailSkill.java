fd4cc2578faad25d5e4ae16408a44c35b5094333
thanhnguyen1121 <thanhnguyen9908@gmail.com>
2019-08-19 09:37:26 +0700

    thành to

---
+++ app/src/main/java/com/thanhnguyen/devjob/Model/ModelJobDetail/ModelJobDetailSkill.java
@@ -0,0 +1,29 @@
+package com.thanhnguyen.devjob.Model.ModelJobDetail;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ModelJobDetailSkill {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
