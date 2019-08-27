546163d9ca8a071358e9bb015414e8003434d4a0
Thanh-Minh <41420911+Thanh-Minh@users.noreply.github.com>
2019-08-27 08:45:31 +0700

    update

--- app/src/main/java/com/thanhnguyen/devjob/View/ActivityView/JobDetail/JobDetailActivity.java
+++ app/src/main/java/com/thanhnguyen/devjob/View/ActivityView/JobDetail/JobDetailActivity.java
@@ -1,34 +1,26 @@
 package com.thanhnguyen.devjob.View.ActivityView.JobDetail;
 
+
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.print.PrintAttributes;
 import android.text.Html;
-import android.text.InputType;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.NestedScrollView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import androidx.transition.ChangeBounds;
-import androidx.transition.TransitionManager;
-import androidx.transition.TransitionSet;
-
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.thanhnguyen.devjob.Adapter.AdapterJobRelate;
 import com.thanhnguyen.devjob.Model.ModelJobDetail.ModeJobDetailCompany;
 import com.thanhnguyen.devjob.Model.ModelJobDetail.ModelJobDetailDetail;
@@ -41,6 +33,15 @@ import com.thanhnguyen.devjob.Utils.Constant;
 
 import java.util.List;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.transition.ChangeBounds;
+import androidx.transition.TransitionManager;
+import androidx.transition.TransitionSet;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -48,14 +49,28 @@ import butterknife.OnClick;
 public class JobDetailActivity extends AppCompatActivity implements JobDetailViewImp,
         ItemRcvClickListener, GestureDetector.OnGestureListener, SwipeRefreshLayout.OnRefreshListener {
 
-    @BindView(R.id.jobs_detail_txtName)
-    TextView jobsDetailTxtName;
-    @BindView(R.id.jobs_detail_txtTitle)
-    TextView jobsDetailTxtTitle;
-    @BindView(R.id.jobs_detail_txtTag1)
-    TextView jobsDetailTxtTag1;
+
+
     @BindView(R.id.jobs_detail_txtLocation)
     TextView jobsDetailTxtLocation;
+    @BindView(R.id.jobs_detail_txtTag1)
+    TextView jobsDetailTxtTag1;
+    @BindView(R.id.ln_jobs_detail_address)
+    LinearLayout lnJobsDetailAddress;
+    @BindView(R.id.jobs_detail_txtName)
+    TextView jobsDetailTxtName;
+    @BindView(R.id.jobs_detail_imgback)
+    ImageView jobsDetailImgback;
+    @BindView(R.id.jobs_detail_txtTitle)
+    TextView jobsDetailTxtTitle;
+    @BindView(R.id.rl_jobs_detail_header)
+    LinearLayout rlJobsDetailHeader;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout toolbarLayout;
+    @BindView(R.id.rl_job_detail_header)
+    AppBarLayout rlJobDetailHeader;
     @BindView(R.id.jobs_detail_txtBonus)
     TextView jobsDetailTxtBonus;
     @BindView(R.id.jobs_detail_txtSalary)
@@ -64,29 +79,24 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
     TextView jobsDetailTxtJobType;
     @BindView(R.id.jobs_detail_txtDeadline)
     TextView jobsDetailTxtDeadline;
+    @BindView(R.id.body)
+    LinearLayout body;
     @BindView(R.id.jobs_detail_txtDescription)
     TextView jobsDetailTxtDescription;
     @BindView(R.id.jobs_detail_txtBenefit)
     TextView jobsDetailTxtBenefit;
     @BindView(R.id.jobs_detail_txtRequirement)
     TextView jobsDetailTxtRequirement;
-    @BindView(R.id.jobs_detail_imgback)
-    ImageView jobsDetailImgback;
-    @BindView(R.id.ln_jobs_detail_address)
-    LinearLayout lnJobsDetailAddress;
-    @BindView(R.id.rl_job_detail_header)
-    RelativeLayout rlJobDetailHeader;
     @BindView(R.id.job_detail_rcv_job_relate)
     RecyclerView jobDetailRcvJobRelate;
     @BindView(R.id.jobs_detail_scollview)
     NestedScrollView scrollView;
+    @BindView(R.id.job_detail_apply)
+    Button jobDetailApply;
+    @BindView(R.id.job_detail_save)
+    Button jobDetailSave;
     @BindView(R.id.ln_controll_button)
     LinearLayout lnControll;
-    @BindView(R.id.ln_main_job_main)
-    RelativeLayout rlMainJobMain;
-    @BindView(R.id.swipe_refresh_job_detail)
-    SwipeRefreshLayout swipeRefesh;
-
     private JobDetailPresenterImp jobDetailPresenterImp;
     private List<ModelJobDetailJobRelate> relateList;
     private int Ymin = 10, speed = 400;
@@ -98,8 +108,7 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_detail);
         ButterKnife.bind(this);
-        rlMainJobMain.setVisibility(View.GONE);
-        swipeRefesh.setRefreshing(true);
+
         Intent intent = getIntent();
         String slug = intent.getStringExtra("slug");
         jobDetailPresenterImp = new JobDetailPresenterLogic(this);
@@ -133,60 +142,8 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
                 }
             }
         });
-        swipeRefesh.setOnRefreshListener(this);
-    }
-
-
-    @Override
-    public void getData(ModelJobDetailDetail detail, ModeJobDetailCompany company,
-                        List<ModelJobDetailJobRelate> relateList) {
-        if (detail != null) {
-            swipeRefesh.setRefreshing(false);
-            rlMainJobMain.setVisibility(View.VISIBLE);
-            jobsDetailTxtName.setText(company.getName());
-            jobsDetailTxtTitle.setText(detail.getTitle());
-            jobsDetailTxtTag1.setText(detail.getName());
-            jobsDetailTxtLocation.setText(detail.getAddress());
-            jobsDetailTxtBonus.setText(detail.getBonus() + "$");
-            if (detail.getSalaryMin() != 0 && detail.getSalaryMax() != 0) {
-                jobsDetailTxtSalary.setText(detail.getSalaryMin() + "~" + detail.getSalaryMax() + "$");
-            } else {
-                jobsDetailTxtSalary.setText("None");
-            }
-
-            if (detail.getType() == 1) {
-                jobsDetailTxtJobType.setText("FullTime");
-
-            } else {
-                jobsDetailTxtJobType.setText("PartTime");
-            }
-
-            jobsDetailTxtDeadline.setText(Constant.xuLyThoiGian(detail.getStartTime()));
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-
-                jobsDetailTxtDescription.setText(Html.fromHtml(detail.getDescription(), Html.FROM_HTML_MODE_COMPACT));
-                jobsDetailTxtBenefit.setText(Html.fromHtml(detail.getBenefit(), Html.FROM_HTML_MODE_COMPACT));
-                jobsDetailTxtRequirement.setText(Html.fromHtml(detail.getRequirement(), Html.FROM_HTML_MODE_COMPACT));
-            } else {
-
-
-                jobsDetailTxtDescription.setText(Html.fromHtml(detail.getDescription()));
-                jobsDetailTxtBenefit.setText(Html.fromHtml(detail.getBenefit()));
-                jobsDetailTxtRequirement.setText(Html.fromHtml(detail.getRequirement()));
-
-            }
-
-            createRcvRelateJob(relateList);
-
-        }
-    }
-
-    @Override
-    public void getError(String mes) {
 
     }
-
-
     @OnClick({R.id.jobs_detail_imgback, R.id.job_detail_apply, R.id.job_detail_save})
     public void onViewClicked(View view) {
         switch (view.getId()) {
@@ -201,24 +158,6 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
                 break;
         }
     }
-
-    private void createRcvRelateJob(List<ModelJobDetailJobRelate> raRelateList) {
-        this.relateList = raRelateList;
-        AdapterJobRelate adapterJobRelate = new AdapterJobRelate(raRelateList, this);
-        LinearLayoutManager manager = new LinearLayoutManager(this);
-        manager.setOrientation(RecyclerView.HORIZONTAL);
-        jobDetailRcvJobRelate.setLayoutManager(manager);
-        jobDetailRcvJobRelate.setAdapter(adapterJobRelate);
-    }
-
-
-    @Override
-    public void clickedItem(int position) {
-        Intent intent = new Intent(this, JobDetailActivity.class);
-        intent.putExtra("slug", relateList.get(position).getSlug());
-        startActivity(intent);
-    }
-
 
     @Override
     public boolean onDown(MotionEvent e) {
@@ -259,12 +198,12 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
             RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams((
                     int) RelativeLayout.LayoutParams.WRAP_CONTENT,
                     (int) RelativeLayout.LayoutParams.WRAP_CONTENT);
-            params.leftMargin = 20;
+            params.leftMargin = 0;
             params.topMargin = 20;
             params.bottomMargin = 20;
             params.rightMargin = 20;
             params.addRule(RelativeLayout.CENTER_VERTICAL);
-            jobsDetailImgback.setLayoutParams(params);
+            //jobsDetailImgback.setLayoutParams(params);
 
             jobsDetailTxtName.setVisibility(View.GONE);
             lnJobsDetailAddress.setVisibility(View.GONE);
@@ -281,6 +220,67 @@ public class JobDetailActivity extends AppCompatActivity implements JobDetailVie
 
     @Override
     public void onRefresh() {
-        swipeRefesh.setRefreshing(false);
+
+    }
+
+    @Override
+    public void clickedItem(int position) {
+
+    }
+
+
+    @Override
+    public void getData(ModelJobDetailDetail detail, ModeJobDetailCompany company,
+                        List<ModelJobDetailJobRelate> relateList) {
+        if (detail != null) {
+            jobsDetailTxtName.setText(company.getName());
+            jobsDetailTxtTitle.setText(detail.getTitle());
+            jobsDetailTxtTag1.setText(detail.getName());
+            jobsDetailTxtLocation.setText(detail.getAddress());
+            jobsDetailTxtBonus.setText(detail.getBonus() + "$");
+            if (detail.getSalaryMin() != 0 && detail.getSalaryMax() != 0) {
+                jobsDetailTxtSalary.setText(detail.getSalaryMin() + "~" + detail.getSalaryMax() + "$");
+            } else {
+                jobsDetailTxtSalary.setText("None");
+            }
+
+            if (detail.getType() == 1) {
+                jobsDetailTxtJobType.setText("FullTime");
+
+            } else {
+                jobsDetailTxtJobType.setText("PartTime");
+            }
+
+            jobsDetailTxtDeadline.setText(Constant.xuLyThoiGian(detail.getStartTime()));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+                jobsDetailTxtDescription.setText(Html.fromHtml(detail.getDescription(), Html.FROM_HTML_MODE_COMPACT));
+                jobsDetailTxtBenefit.setText(Html.fromHtml(detail.getBenefit(), Html.FROM_HTML_MODE_COMPACT));
+                jobsDetailTxtRequirement.setText(Html.fromHtml(detail.getRequirement(), Html.FROM_HTML_MODE_COMPACT));
+            } else {
+
+
+                jobsDetailTxtDescription.setText(Html.fromHtml(detail.getDescription()));
+                jobsDetailTxtBenefit.setText(Html.fromHtml(detail.getBenefit()));
+                jobsDetailTxtRequirement.setText(Html.fromHtml(detail.getRequirement()));
+
+            }
+
+            createRcvRelateJob(relateList);
+
+        }
+    }
+    private void createRcvRelateJob(List<ModelJobDetailJobRelate> raRelateList) {
+        this.relateList = raRelateList;
+        AdapterJobRelate adapterJobRelate = new AdapterJobRelate(raRelateList, this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(RecyclerView.HORIZONTAL);
+        jobDetailRcvJobRelate.setLayoutManager(manager);
+        jobDetailRcvJobRelate.setAdapter(adapterJobRelate);
+    }
+
+    @Override
+    public void getError(String mes) {
+
     }
 }
